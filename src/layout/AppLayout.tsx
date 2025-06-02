af73f228b15f80b9f43e759d11cedd76b79deb2a
srikarsesham <srikar.sesham@meridianit.com>
2025-06-02 11:17:51 +0530

    fixed-applayout

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,40 +1,43 @@
 import { SidebarProvider, useSidebar } from "../context/SidebarContext";
 import { Outlet } from "react-router";
 import AppHeader from "./Navbar";
-
 import AppSidebar from "./AppSidebar";
 
 const LayoutContent: React.FC = () => {
   const { isExpanded, isHovered, isMobileOpen } = useSidebar();
 
   return (
-    <div className="min-h-screen flex flex-col">
-      {/* Navbar at the top */}
-      <AppHeader />
+    <div className="relative min-h-screen">
+      {/* Navbar - fixed at top */}
+      <div className="fixed top-0 left-0 right-0 z-40">
+        <AppHeader />
+      </div>
 
-      {/* Container that starts BELOW the Navbar */}
-      <div className="flex flex-1 h-[calc(100vh-64px)]">
-        {/* Sidebar */}
-        <aside
-          className={`bg-white dark:bg-gray-900 border-r border-gray-200 dark:border-gray-800
+      {/* Sidebar - fixed below Navbar */}
+      <aside
+        className={`
+          fixed top-20 left-0 h-[calc(100vh-64px)] z-30 bg-white dark:bg-gray-900 border-r border-gray-200 dark:border-gray-800
           transition-all duration-300 ease-in-out
           ${isExpanded || isHovered ? "lg:w-[290px]" : "lg:w-[90px]"}
-          ${isMobileOpen ? "fixed top-16 left-0 w-[290px] h-[calc(100vh-64px)] z-50" : "hidden lg:block"}`}
-        >
-          <AppSidebar />
+          ${isMobileOpen ? "w-[290px]" : "hidden lg:block"}
+        `}
+      >
+        <AppSidebar />
+      </aside>
 
-          {/* <Backdrop /> */}
-        </aside>
-
-        {/* Main Content */}
-        <main
-          className={`flex-1 p-4 mx-auto max-w-[--breakpoint-2xl] md:p-6
+      {/* Main content area - pushed by sidebar */}
+      <main
+        className={`
+          pt-16
           transition-all duration-300 ease-in-out
-          ${isExpanded || isHovered ? "lg:ml-0" : "lg:ml-0"}`}
-        >
+          ${isExpanded || isHovered ? "lg:ml-[290px]" : "lg:ml-[90px]"}
+          ${isMobileOpen ? "ml-0" : ""}
+        `}
+      >
+        <div className="p-4 mx-auto max-w-[--breakpoint-2xl] md:p-6">
           <Outlet />
-        </main>
-      </div>
+        </div>
+      </main>
     </div>
   );
 };
